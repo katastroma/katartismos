fbe22cbf6716e483d34291775b91ba75c3beff35
Nathan Blair <me@nathanblair.rocks>
2026-03-31 13:02:01 -0400

    chore(docs): add doc comments for exported symbols

--- go/labels.go
+++ go/labels.go
@@ -1,8 +1,13 @@
+//revive:disable:package-comments
 package katartismos
 
 const (
+	// TenantLabel is the label where the tenant information is kept
 	TenantLabel = "katastroma.org/tenant"
-	RepoLabel   = "katastroma.org/repo"
-	RefLabel    = "katastroma.org/ref"
-	PathLabel   = "katastroma.org/path"
+	// RepoLabel is the label where the repo information is kept
+	RepoLabel = "katastroma.org/repo"
+	// RefLabel is the label where the ref information is kept
+	RefLabel = "katastroma.org/ref"
+	// PathLabel is the label where the path information is kept
+	PathLabel = "katastroma.org/path"
 )
